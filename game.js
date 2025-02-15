b17e2120745df414dba58fd4f5221c458a3d56ac
Shubham-Chakraborty <chakrabortyshubham00@gmail.com>
2025-02-15 16:19:51 +0000

    Add function playGame() and put playRound() inside
    Added the final function playGame() and moved the playRound() function
    along with related variables inside the playGame() function. This
    function playGame() calls the playRound function 5 times and prints
    appropriate message after every round and a final message after the end
    of the 5 rounds.

--- game.js
+++ game.js
@@ -17,52 +17,105 @@ function getHumanChoice() {
     return humanChoice.toLowerCase();
 }
 
-let humanScore = 0;
-let computerScore = 0;
 
-function playRound(computerChoice, humanChoice) {
-    if (humanChoice == 'rock') {
-        if (computerChoice == 'rock') {
-            console.log("Round Drawn! Rock ties Rock.");
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    let humanChoice, computerChoice;
+
+    function playRound(computerChoice, humanChoice) {
+        if (humanChoice == 'rock') {
+            if (computerChoice == 'rock') {
+                console.log("Round Drawn! Rock ties Rock.");
+            }
+            else if (computerChoice == 'paper') {
+                console.log("You Loose! Paper beats Rock.");
+                ++computerScore;
+            }
+            else {
+                console.log("You Win! Rock beats Scissor");
+                ++humanScore;
+            }
         }
-        else if (computerChoice == 'paper') {
-            console.log("You Loose! Paper beats Rock.");
-            ++computerScore;
+        else if (humanChoice == 'paper') {
+            if (computerChoice == 'rock') {
+                console.log("You Win! Paper beats Rock.");
+                ++humanScore;
+            }
+            else if (computerChoice == 'paper') {
+                console.log("Round Drawn! Paper ties Paper.");
+            }
+            else {
+                console.log("You Loose! Scissor beats Paper");
+                ++computerScore;
+            }
         }
-        else {
-            console.log("You Win! Rock beats Scissor");
-            ++humanScore;
+        if (humanChoice == 'scissor') {
+            if (computerChoice == 'rock') {
+                console.log("You Loose! Rock beats Scissor.");
+                ++computerScore;
+            }
+            else if (computerChoice == 'paper') {
+                console.log("You Win! Scissor beats Paper.");
+                ++humanScore;
+            }
+            else {
+                console.log("Round Drawn! Scissor ties Scissor");
+            }
         }
     }
-    else if (humanChoice == 'paper') {
-        if (computerChoice == 'rock') {
-            console.log("You Win! Paper beats Rock.");
-            ++humanScore;
-        }
-        else if (computerChoice == 'paper') {
-            console.log("Round Drawn! Paper ties Paper.");
-        }
-        else {
-            console.log("You Loose! Scissor beats Paper");
-            ++computerScore;
-        }
+
+
+    console.log("--------------ROUND 1--------------")
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+    playRound(computerChoice, humanChoice);
+    console.log("-----------END OF ROUND 1-----------");
+    console.log("Results : \nYour Score: "+humanScore+" Computer Score: "+computerScore);
+
+    console.log("--------------ROUND 2--------------")
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+    playRound(computerChoice, humanChoice);
+    console.log("-----------END OF ROUND 2-----------");
+    console.log("Results : \nYour Score: "+humanScore+" Computer Score: "+computerScore);
+    
+    console.log("--------------ROUND 3--------------")
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+    playRound(computerChoice, humanChoice);
+    console.log("-----------END OF ROUND 3-----------");
+    console.log("Results : \nYour Score: "+humanScore+" Computer Score: "+computerScore);
+    
+    console.log("--------------ROUND 4--------------")
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+    playRound(computerChoice, humanChoice);
+    console.log("-----------END OF ROUND 4-----------");
+    console.log("Results : \nYour Score: "+humanScore+" Computer Score: "+computerScore);
+    
+    console.log("--------------ROUND 5--------------")
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+    playRound(computerChoice, humanChoice);
+    console.log("-----------END OF ROUND 5-----------");
+    console.log("Results : \nYour Score: "+humanScore+" Computer Score: "+computerScore);
+    
+    if (humanScore > computerScore) {
+        console.log(`***************************************************************
+                                                YOU WIN
+                    ***************************************************************`);
     }
-    if (humanChoice == 'scissor') {
-        if (computerChoice == 'rock') {
-            console.log("You Loose! Rock beats Scissor.");
-            ++computerScore;
-        }
-        else if (computerChoice == 'paper') {
-            console.log("You Win! Scissor beats Paper.");
-            ++humanScore;
-        }
-        else {
-            console.log("Round Drawn! Scissor ties Scissor");
-        }
+    else if (computerScore = humanScore) {
+        console.log(`***************************************************************
+                                                YOU DRAW
+                    ***************************************************************`);
+    }
+    else {
+        console.log(`***************************************************************
+                                                YOU LOOSE
+                    ***************************************************************`);
     }
 }
 
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
-
-playRound(computerChoice, humanChoice);
+playGame();
